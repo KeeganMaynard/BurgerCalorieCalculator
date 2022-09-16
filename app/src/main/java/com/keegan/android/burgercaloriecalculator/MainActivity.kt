934790337c68899e14852a0132ebffa07b12b404
Keegan <kgm1328@live.missouristate.edu>
2022-09-16 12:04:56 -0500

    Added UI functionality to MainActivity.kt

--- app/src/main/java/com/keegan/android/burgercaloriecalculator/MainActivity.kt
+++ app/src/main/java/com/keegan/android/burgercaloriecalculator/MainActivity.kt
@@ -2,10 +2,38 @@ package com.keegan.android.burgercaloriecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var burger: Burger
+    private lateinit var pattyRG : RadioGroup
+    private lateinit var prosciuttoCB : CheckBox
+    private lateinit var cheeseRG : RadioGroup
+    private lateinit var sauceSBR : SeekBar
+    private lateinit var caloriesTV : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        burger = Burger()
+        initialize()
+    }
+
+    private fun initialize(){
+        pattyRG = findViewById(R.id.radioGroup1)
+        prosciuttoCB = findViewById(R.id.checkBox1)
+        cheeseRG = findViewById(R.id.radioGroup2)
+        sauceSBR = findViewById(R.id.seekBar1)
+        caloriesTV = findViewById(R.id.textView2)
+
+        displayCalories()
+    }
+
+    private fun displayCalories(){
+        var calorieText = "Calories: " + burger.totalCalories
+        caloriesTV.setText(calorieText)
     }
 }
